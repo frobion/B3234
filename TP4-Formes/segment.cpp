c832bf5cd22d57ca7fe8f2a46cd83490f1e6a3f5
nathan-insa <nathan.arsac@insa-lyon.fr>
2016-01-17 16:01:02 +0100

    Ajout de getInformation pour rectangle et segment

--- TP4-Formes/segment.cpp
+++ TP4-Formes/segment.cpp
@@ -66,7 +66,8 @@ bool Segment::Hit(Point p)
 
 string Segment::GetInformation()
 {
-    return "";
+    string information = "S " + name + " " + extremity1.GetX() + " " + extremity1.GetY() + " " + extremity2.GetX() + " " + extremity2.GetY() ;
+    return information;
 }
 
 //------------------------------------------------------- Surcharge d'operateurs
